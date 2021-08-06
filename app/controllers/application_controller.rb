f8f2e99c9821bae8bd177072d640ab7a25ce8f05
KravchenkoAndrey24 <yazaandrey@gmail.com>
2021-08-06 13:31:59 +0300

    changed root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+                def index
+                end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'api/v1/todo_items#index'
+  root 'application#index'
 
   devise_for :users
   namespace :api do
