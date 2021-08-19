748adf548baecd4ffd14e3699c1e2dcb13bbe42b
KravchenkoAndrey24 <yazaandrey@gmail.com>
2021-08-19 18:38:56 +0300

    change cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,13 +3,15 @@
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 # origins 'https://cocky-kare-ad2c13.netlify.app'
+# origins 'http://localhost:3001'
 
 # Read more: https://github.com/cyu/rack-cors
 
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+
+    origins 'https://cocky-kare-ad2c13.netlify.app'
 
     resource '*',
              headers: :any,
